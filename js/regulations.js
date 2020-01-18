6e9202572985a7d6b49597c3ceb367215cbdf05b
cornzz <39997278+cornzz@users.noreply.github.com>
2020-01-18 20:11:40 +0100

    Update regulations EN, add biography

--- js/regulations.js
+++ js/regulations.js
@@ -1,8 +1,17 @@
-let headerheight = $('.masthead').height();
-$('#spacer').height(headerheight);
+// Adapt spacer on pageload and window resize
+function init() {
+	let headerheight = $('.masthead').height();
+	$('#spacer').height(headerheight);
+}
+init();
+$(window).resize(init);
+
+// Jump to requested location
 let hashpos = ($(window.location.hash).offset()) ? $(window.location.hash).offset().top : 0; 
 $(window).scrollTop(hashpos - headerheight);
 $('#lang').attr('href', '/regulations' + window.location.hash);
+
+// Prevent animations on pageload
 window.setTimeout(function () {$('body').removeClass('preload'), 150});
 
 function mail(arg) {
