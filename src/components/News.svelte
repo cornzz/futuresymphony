1b080cd012bf891e9bbadb687b9ae8b1292265cf
cornzz <39997278+cornzz@users.noreply.github.com>
2022-01-17 18:34:20 +0100

    Fix sponsor layout, add latga
    Fix tabs, news layout
    Remove old logos
    Fix some font sizes

--- src/components/News.svelte
+++ src/components/News.svelte
@@ -14,8 +14,8 @@
 </script>
 
 {#each articles as { article }}
-    <a href={`/news/${article.slug}`} class="news-link">
-        <div class="news-item dropshadow" class:fixed={!latest}>
+    <a href={`/news/${article.slug}`} class="news-link" class:fixed={!latest}>
+        <div class="news-item dropshadow">
             <img class="news-image-small" src={`/images/news/${article.images.small}`} alt={article.images.small}>
             <div class="news-content" data-readmore="{$_('news.readmore')}">
                 <div class="date"><i>{@html article.date[$locale]}</i></div>
@@ -35,6 +35,71 @@
     .news-link
         text-decoration none
         color inherit
+        height 27%
+        margin-bottom 1.3%
+
+        .news-item
+            display flex
+            height 100%
+            border 1px solid #CCC
+            border-radius 2px
+
+            img
+                height 100%
+
+            .news-content
+                position relative
+                display grid
+                grid-template-rows 1fr 1fr 3fr
+                padding 5px 10px 10px
+                
+                & > *
+                    overflow hidden
+                    text-overflow ellipsis
+
+                .date
+                    font-size 0.85em
+                    color rgba(100, 100, 100, 0.5)
+
+                .title
+                    white-space nowrap
+                    overflow hidden
+
+                .text
+                    line-height: 1.55
+
+                &:before
+                    content ''
+                    position absolute
+                    top 0
+                    bottom 0
+                    left 0
+                    right 0
+                    opacity 0
+                    background-color #F5F5F5EF
+                    border-top-right-radius 2px
+                    border-bottom-right-radius 2px
+                    transition all 0.3s ease
+                
+                &:after
+                    content attr(data-readmore)
+                    position absolute
+                    top 50%
+                    left 0
+                    right 0
+                    transform translateY(-70%)
+                    font-weight 700
+                    opacity 0
+                    text-align center
+                    color var(--color-link)
+                    transition all 0.3s ease
+
+            &:hover .news-content:before
+            &:hover .news-content:after
+                opacity 1
+            
+            &:hover .news-content:after
+                transform translateY(-50%)
 
     .dropshadow
         transition all 0.1s ease
@@ -42,70 +107,6 @@
         &:hover
             cursor pointer
             box-shadow 0px 0px 4px 1px var(--color-boxshadow)
-
-    .news-item
-        display flex
-        height 26%
-        margin-bottom 1.3%
-        border 1px solid #CCC
-        border-radius 2px
-
-        img
-            height 100%
-
-        .news-content
-            position relative
-            display grid
-            grid-template-rows 1fr 1fr 3fr
-            padding 5px 10px 10px
-            
-            & > *
-                overflow hidden
-                text-overflow ellipsis
-
-            .date
-                font-size 0.85em
-                color rgba(100, 100, 100, 0.5)
-
-            .title
-                white-space nowrap
-                overflow hidden
-
-            .text
-                line-height: 1.55
-
-            &:before
-                content ''
-                position absolute
-                top 0
-                bottom 0
-                left 0
-                right 0
-                opacity 0
-                background-color #F5F5F5EF
-                border-top-right-radius 2px
-                border-bottom-right-radius 2px
-                transition all 0.3s ease
-            
-            &:after
-                content attr(data-readmore)
-                position absolute
-                top 50%
-                left 0
-                right 0
-                transform translateY(-70%)
-                font-weight 700
-                opacity 0
-                text-align center
-                color var(--color-link)
-                transition all 0.3s ease
-
-        &:hover .news-content:before
-        &:hover .news-content:after
-            opacity 1
-        
-        &:hover .news-content:after
-            transform translateY(-50%)
     
     .fixed
         height 25vh
--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -2,64 +2,88 @@
     import { _ } from 'svelte-i18n'
 </script>
 
-<h2 class="cover-subheading">{$_('sponsors.sponsors')}:</h2>
-<div class="flex-container">
-    <div class="flex-item"><a href="https://www.ltkt.lt/" target="_blank"><img src="/images/sponsors/logo_kt.png" alt="logo_kt.png"></a></div>
-    <div class="flex-item"><a href="https://skfoundation.com/" target="_blank"><img src="/images/sponsors/logo_kf.png" alt="logo_kf.png"></a></div>
-</div>
-<h2 class="cover-subheading">{$_('sponsors.partners')}:</h2>
-<div class="flex-container">
-    <div class="flex-item"><a href="https://www.lrt.lt/" target="_blank"><img src="/images/sponsors/logo_lrt.png" alt="logo_lrt.png"></a></div>
-    <div class="flex-item"><a href="https://www.filharmonija.lt/" target="_blank"><img src="/images/sponsors/logo_lnf.png" alt="logo_lnf.png"></a></div>
-    <div class="flex-item"><a href="https://vilnius.lt/en/" target="_blank"><img src="/images/sponsors/logo_v.png" alt="logo_v.png"></a></div>
-    <div style="width: 100%;"></div>
-    <div class="flex-item"><span class="helper"></span><a href="http://www.triplenode.com/" target="_blank"><img src="/images/sponsors/logo_tn.png" alt="logo_tn.png" style="max-height: 48%"></a></div>
-    <div style="width: 100%;"></div>
-    <div class="flex-item"><span class="helper"></span><a href="http://bmkkc.lt/" target="_blank"><img src="/images/sponsors/logo_bmkkc.png" alt="logo_bmkkc.png" style=" max-height: 80%"></a></div>
-    <div class="flex-item"><a href="https://domusmaria.com/" target="_blank"><img src="/images/sponsors/logo_dm.png" alt="logo_dm.png"></a></div>
-    <div class="flex-item"><a href="http://www.cmm.lt/" target="_blank"><img src="/images/sponsors/logo_cmm.png" alt="logo_cmm.png"></a></div>
+<div class="content">
+    <div class="section">
+        <h2 class="cover-subheading">{$_('sponsors.sponsors')}:</h2>
+        <div class="section-content">
+            <div class="sponsor-logo"><a href="https://www.ltkt.lt/" target="_blank"><img src="/images/sponsors/logo_ltkt.png" alt="logo_ltkt.png"></a></div>
+            <div class="sponsor-logo"><a href="https://skfoundation.com/" target="_blank"><img src="/images/sponsors/logo_kf.png" alt="logo_kf.png"></a></div>
+        </div>
+    </div>
+    <div class="section partners">
+        <h2 class="cover-subheading">{$_('sponsors.partners')}:</h2>
+        <div class="section-content">
+            <div class="sponsor-logo"><a href="https://www.lrt.lt/" target="_blank"><img src="/images/sponsors/logo_lrt.png" alt="logo_lrt.png"></a></div>
+            <div class="sponsor-logo"><a href="https://www.filharmonija.lt/" target="_blank"><img src="/images/sponsors/logo_lnf.png" alt="logo_lnf.png"></a></div>
+            <div class="sponsor-logo"><a href="https://vilnius.lt/en/" target="_blank"><img src="/images/sponsors/logo_v.png" alt="logo_v.png"></a></div>
+            <div class="divider"></div>
+            <div class="sponsor-logo"><a href="http://www.triplenode.com/" target="_blank" class="oversize"><img src="/images/sponsors/logo_tn.png" alt="logo_tn.png"></a></div>
+            <div class="sponsor-logo"><a href="https://www.latga.lt/" target="_blank" class="oversize"><img src="/images/sponsors/logo_latga.png" alt="logo_latga.png"></a></div>
+            <div class="divider"></div>
+            <div class="sponsor-logo"><a href="http://bmkkc.lt/" target="_blank"><img src="/images/sponsors/logo_bmkkc.png" alt="logo_bmkkc.png" style=" max-height: 80%"></a></div>
+            <div class="sponsor-logo"><a href="https://domusmaria.com/" target="_blank"><img src="/images/sponsors/logo_dm.png" alt="logo_dm.png"></a></div>
+            <div class="sponsor-logo"><a href="http://www.cmm.lt/" target="_blank"><img src="/images/sponsors/logo_cmm.png" alt="logo_cmm.png"></a></div>
+        </div>
+    </div>
 </div>
 
 <style lang="stylus">
-    .flex-container
+    .content
         display flex
-        flex-flow row wrap
-        justify-content space-evenly
-        /*border 1px solid black*/
-
-    .flex-item
-        height 75px
-        margin-bottom 35px
-        overflow hidden
-        /*border 1px solid #aaa*/
-
-    img
-        max-height 100%
-        filter grayscale(100%)
-        opacity 0.55
-        z-index 0
-        transition filter 0.15s ease-in-out, opacity 0.15s ease-in-out 
-
-    img:hover
-        filter none
-        opacity 1
-
-    .helper
-        display inline-block
+        flex-direction column
         height 100%
-        vertical-align middle
+
+        .section
+            display flex
+            flex-direction column
+
+        .partners
+            flex-grow 1
+
+
+        .section-content
+            display flex
+            flex-flow row wrap
+            justify-content space-around
+            height 100%
+            padding 20px 0
+            align-items center
+            /*border 1px solid black*/
+
+            .sponsor-logo
+                height 85px
+                display flex
+                flex-direction column
+                justify-content center
+                /*border 1px solid #aaa*/
+
+                a
+                    height 100%
+
+                .oversize
+                    height 48%
+
+                img
+                    max-height 100%
+                    opacity 0.55
+                    transition all 0.15s ease-in-out
+
+                img:hover
+                    opacity 1
+
+            .divider
+                width 100%
 
     @media (min-width: 1400px)
-        .flex-item
+        .sponsor-logo
             height 85px
 
     @media (min-width: 1600px) and (min-height: 900px)
-        .flex-item
+        .sponsor-logo
             height 110px
 
     @media screen and (max-device-width: 600px)
-        .flex-item
-            margin-bottom 100px
+        .sponsor-logo
             height 135px
 </style>
 
