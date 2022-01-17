b173102ede0e02168c72192b88eb10590772ab67
cornzz <39997278+cornzz@users.noreply.github.com>
2022-01-17 23:09:04 +0100

    Add bio

---
+++ src/routes/adomas.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import Tab from '../components/Tab.svelte'
+    import { showLanding } from '../helpers/stores'
+
+    let imageFrame
+
+    showLanding.set(false)
+
+    function toggleImage(){
+        imageFrame.classList.toggle('active')
+    }
+</script>
+
+<div class="image-frame" bind:this={imageFrame}  on:click={toggleImage}>
+    <img src="/images/adomas_morkunas_budrys.jpg" alt="Adomas Morkunas Budrys">
+    <span class="image-frame-close"></span>
+</div>
+
+<Tab oversize>
+    <h1 class="cover-heading"><b>Adomas Morkūnas-Budrys</b></h1>
+    <div>
+        <div on:click={toggleImage}>
+            <img class="bio-image" src='/images/adomas_morkunas_budrys_small.jpg' alt='Adomas Morkunas Budrys'>
+        </div>
+        <p>Acclaimed for his analytical mind, charisma, and profound musical understanding, <b>Adomas Morkūnas-Budrys</b> stands out as one of the most promising orchestra conductors of his generation.</p>
+        <p>A 23-year-old conductor Adomas has already worked with such orchestras as Thessaloniki State Symphony, Olten Philharmonic of Izmir, Südwestdeutsche Philharmonie Konstanz, Musikkolegium Winterthur, and MÁV Symphony. In masterclasses, he has had an opportunity to work with such conductors as Paavo Järvi, Bernard Haitink, Mirga Gražinytė-Tyla, Jorma Panula, Larry Rachleff, and James Lowe.</p>
+        <p>Adomas has been studying orchestra conducting with Johannes Schlaefli (Zürich University of the Arts) and prof. Ole Kristian Ruud (Norwegian Academy of Music). Recently, Adomas’ concert in Zürich in which he presented all of Jean Sibelius’s symphonies was awarded the highest mark.</p>
+        <p>Alongside the conductor’s career, Adomas is an active pianist, composer, and organizer of various musical events. As a composer, Adomas won the 1st prize at the National Competition of Young Composers "Mano nata". His compositions were performed by various ensembles, including the chamber choir “Aidija”, “Jauna Muzika”, and New Ideas Chamber Orchestra NICO. His piece for piano solo <i>“An Extraordinary Eggplant’s with the Bolognese Sause Night Walk in the Stomach”</i> was included in the concert tour program "Advent Calendar: 24 Encores" by the pianist Gintaras Januševičius and was performed in Zürich, New York, Rio de Janeiro, various German and Lithuanian cities. Adomas attended masterclasses and received invaluable composition experience from Gediminas Gelgotas and Peter Michael Hamel.</p>
+        <p>Since 2018, Adomas is a co-organizer of the Future Symphony Competition. Being a chief conductor of the Future Symphony Orchestra, he has already premiered 14 orchestral compositions.</p>
+    </div>
+</Tab>
+
+<style lang="stylus">
+    @require '../styles/image-frame.styl'
+
+    .bio-image
+        float left
+        max-height 50vh
+        margin 10px 20px 10px 0
+
+    .bio-image:hover
+        cursor pointer
+</style>
