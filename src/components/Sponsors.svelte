3e4a62d34d5cb37b9462859ae12cd969126cfdfb
cornzz <39997278+cornzz@users.noreply.github.com>
2022-01-19 19:57:18 +0100

    Fix sponsors layout

--- src/components/Sponsors.svelte
+++ src/components/Sponsors.svelte
@@ -5,24 +5,26 @@
 <div class="content">
     <div class="section">
         <h2 class="cover-subheading">{$_('sponsors.sponsors')}:</h2>
-        <div class="section-content">
-            <div class="sponsor-logo"><a href="https://www.ltkt.lt/" target="_blank"><img src="/images/sponsors/logo_ltkt.png" alt="logo_ltkt.png"></a></div>
-            <div class="sponsor-logo"><a href="https://skfoundation.com/" target="_blank"><img src="/images/sponsors/logo_kf.png" alt="logo_kf.png"></a></div>
+        <div class="section-row">
+            <a href="https://www.ltkt.lt/" target="_blank"><img src="/images/sponsors/logo_ltkt.png" alt="logo_ltkt.png"></a>
+            <a href="https://skfoundation.com/" target="_blank"><img src="/images/sponsors/logo_kf.png" alt="logo_kf.png"></a>
         </div>
     </div>
     <div class="section partners">
         <h2 class="cover-subheading">{$_('sponsors.partners')}:</h2>
-        <div class="section-content">
-            <div class="sponsor-logo"><a href="https://www.lrt.lt/" target="_blank"><img src="/images/sponsors/logo_lrt.png" alt="logo_lrt.png"></a></div>
-            <div class="sponsor-logo"><a href="https://www.filharmonija.lt/" target="_blank"><img src="/images/sponsors/logo_lnf.png" alt="logo_lnf.png"></a></div>
-            <div class="sponsor-logo"><a href="https://vilnius.lt/en/" target="_blank"><img src="/images/sponsors/logo_v.png" alt="logo_v.png"></a></div>
-            <div class="divider"></div>
-            <div class="sponsor-logo"><a href="http://www.triplenode.com/" target="_blank" class="oversize"><img src="/images/sponsors/logo_tn.png" alt="logo_tn.png"></a></div>
-            <div class="sponsor-logo"><a href="https://www.latga.lt/" target="_blank" class="oversize"><img src="/images/sponsors/logo_latga.png" alt="logo_latga.png"></a></div>
-            <div class="divider"></div>
-            <div class="sponsor-logo"><a href="http://bmkkc.lt/" target="_blank"><img src="/images/sponsors/logo_bmkkc.png" alt="logo_bmkkc.png" style=" max-height: 80%"></a></div>
-            <div class="sponsor-logo"><a href="https://domusmaria.com/" target="_blank"><img src="/images/sponsors/logo_dm.png" alt="logo_dm.png"></a></div>
-            <div class="sponsor-logo"><a href="http://www.cmm.lt/" target="_blank"><img src="/images/sponsors/logo_cmm.png" alt="logo_cmm.png"></a></div>
+        <div class="section-row">
+            <a href="https://www.lrt.lt/" target="_blank"><img src="/images/sponsors/logo_lrt.png" alt="logo_lrt.png"></a>
+            <a href="https://www.filharmonija.lt/" target="_blank"><img src="/images/sponsors/logo_lnf.png" alt="logo_lnf.png"></a>
+            <a href="https://vilnius.lt/en/" target="_blank"><img src="/images/sponsors/logo_v.png" alt="logo_v.png"></a>
+        </div>
+        <div class="section-row">
+            <a href="http://www.triplenode.com/" target="_blank" class="oversize"><img src="/images/sponsors/logo_tn.png" alt="logo_tn.png"></a>
+            <a href="https://www.latga.lt/" target="_blank"class="oversize"><img src="/images/sponsors/logo_latga.png" alt="logo_latga.png"></a>
+        </div>
+        <div class="section-row">
+            <a href="http://bmkkc.lt/" target="_blank"><img src="/images/sponsors/logo_bmkkc.png" alt="logo_bmkkc.png" style=" max-height: 80%"></a>
+            <a href="https://domusmaria.com/" target="_blank" class="undersize"><img src="/images/sponsors/logo_dm.png" alt="logo_dm.png"></a>
+            <a href="http://www.cmm.lt/" target="_blank"><img src="/images/sponsors/logo_cmm.png" alt="logo_cmm.png"></a>
         </div>
     </div>
 </div>
@@ -37,53 +39,35 @@
             display flex
             flex-direction column
 
-        .partners
-            flex-grow 1
+            &.partners
+                flex-grow 1
 
-
-        .section-content
-            display flex
-            flex-flow row wrap
-            justify-content space-around
-            height 100%
-            padding 20px 0
-            align-items center
-            /*border 1px solid black*/
-
-            .sponsor-logo
-                height 85px
+            .section-row
                 display flex
-                flex-direction column
-                justify-content center
-                /*border 1px solid #aaa*/
+                justify-content space-around
+                height 100%
+                align-items center
+                /*border 1px solid black*/
 
                 a
-                    height 100%
+                    height 10vmin
+                    display flex
+                    flex-direction column
+                    justify-content center
+                    /*border 1px solid #aaa*/
 
-                .oversize
-                    height 48%
+                    &.oversize
+                        height 4.8vmin
 
-                img
-                    max-height 100%
-                    opacity 0.55
-                    transition all 0.15s ease-in-out
+                    &.undersize
+                        height 13vmin
 
-                img:hover
-                    opacity 1
+                    img
+                        max-height 100%
+                        opacity 0.55
+                        transition all 0.15s ease-in-out
 
-            .divider
-                width 100%
-
-    @media (min-width: 1400px)
-        .sponsor-logo
-            height 85px
-
-    @media (min-width: 1600px) and (min-height: 900px)
-        .sponsor-logo
-            height 110px
-
-    @media screen and (max-device-width: 600px)
-        .sponsor-logo
-            height 135px
+                    img:hover
+                        opacity 1
 </style>
 
