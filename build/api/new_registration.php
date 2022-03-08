5b38f26b6114d19f1f3e3832964859db28e056c8
cornzz <39997278+cornzz@users.noreply.github.com>
2022-03-09 00:42:31 +0100

    Rebuild (mailer)

--- build/api/new_registration.php
+++ build/api/new_registration.php
@@ -1,12 +1,22 @@
 <?php
-    include "db_connection.php";
-    include "helpers.php";
+    require_once "load_env.php";
+    require "db_connection.php";
+    require "helpers.php";
+    require "mail.php";
 
     $conn = OpenCon();
 
-    // Prevent spam
-    $client_addr = $_SERVER['REMOTE_ADDR'];
+    // Validate form
+    $form = json_decode(file_get_contents("php://input"), TRUE);
+    if (!ValidateDTO($form, TRUE)) {
+        http_response_code(400);
+        echo "Invalid form.";
+        return;
+    }
+        
+    $client_addr = $_SERVER["REMOTE_ADDR"];
     if (APP_ENV != "dev") {
+        // Prevent spam
         $stmt = $conn->prepare("SELECT * FROM request_addr WHERE address=?");
         $stmt->bind_param("s", $client_addr);
         $stmt->execute();
@@ -18,30 +28,28 @@
                 return;
             }
         }
-    }
-    
-    // Validate form
-    $form = json_decode(file_get_contents("php://input"), TRUE);
-    if (!ValidateDTO($form, TRUE)) {
-        http_response_code(400);
-        echo "Invalid form.";
-        return;
-    }
 
-    // Check if email in use
-    $stmt = $conn->prepare("SELECT * FROM new_registrations WHERE email=?");
-    $stmt->bind_param("s", $form["email"]);
-    $stmt->execute();
-    if (mysqli_num_rows($stmt->get_result())) {
-        http_response_code(400);
-        echo "Email already used.";
-        return;
+        // Check if email in use
+        $stmt = $conn->prepare("SELECT * FROM new_registrations WHERE email=?");
+        $stmt->bind_param("s", $form["email"]);
+        $stmt->execute();
+        if (mysqli_num_rows($stmt->get_result())) {
+            http_response_code(400);
+            echo "Email already used.";
+            return;
+        }
     }
 
     // Generate reg key
     do {
         $reg_key = join("-", str_split(strtoupper(bin2hex(random_bytes(10))), 5));
     } while (mysqli_num_rows($conn->query("SELECT * FROM new_registrations WHERE reg_key='{$reg_key}'")));
+
+    if (!sendNewRegMail($form["email"], $form["firstName"], $form["lastName"], $reg_key, $form["lang"])) {
+        http_response_code(500);
+        echo "Error sending confirmation email.";
+        return;
+    }
 
     // Insert data into new_registrations
     $stmt = $conn->prepare("INSERT INTO new_registrations(reg_key, email, firstName, lastName, dateOfBirth, country) VALUES (?, ?, ?, ?, ?, ?)");
