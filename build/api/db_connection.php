55df49b59fb156831d954beff17892ce88c57f78
cornzz <39997278+cornzz@users.noreply.github.com>
2022-03-08 02:31:36 +0100

    Rebuild (new reg done)

--- build/api/db_connection.php
+++ build/api/db_connection.php
@@ -1,17 +1,17 @@
 <?php
-    $env_path = '.env';
+    $env_path = ".env";
 
     if (!file_exists($env_path)) {
-        throw new \InvalidArgumentException(sprintf('%s does not exist', $env_path));
+        throw new \InvalidArgumentException(sprintf("%s does not exist", $env_path));
     }
 
     $lines = file($env_path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) {
+        if (strpos(trim($line), "#") === 0) {
             continue;
         }
 
-        list($name, $value) = explode('=', $line, 2);
+        list($name, $value) = explode("=", $line, 2);
         $name = trim($name);
         $value = trim($value);
 
@@ -22,7 +22,7 @@
 
     function OpenCon()
     {
-        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die('Connection failed: %s\n'. $conn->error);
+        $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("Connection failed: %s\n". $conn->error);
 
         return $conn;
     }
--- build/api/new_registration.php
+++ build/api/new_registration.php
@@ -7,12 +7,12 @@
     $form = json_decode(file_get_contents("php://input"), TRUE);
     if (!ValidateDTO($form, TRUE)) {
         http_response_code(400);
-        echo "Invalid form";
+        echo "Invalid form.";
         return;
     }
 
     $stmt = $conn->prepare("SELECT * FROM new_registrations WHERE email=?");
-    $stmt->bind_param('s', $form['email']);
+    $stmt->bind_param("s", $form["email"]);
     $stmt->execute();
     if (mysqli_num_rows($stmt->get_result())) {
         http_response_code(400);
@@ -25,7 +25,7 @@
     } while (mysqli_num_rows($conn->query("SELECT * FROM new_registrations WHERE reg_key='{$reg_key}'")));
 
     $stmt = $conn->prepare("INSERT INTO new_registrations(reg_key, email, firstName, lastName, dateOfBirth, country) VALUES (?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param('ssssss', $reg_key, $form['email'], $form['firstName'], $form['lastName'], $form['dateOfBirth'], $form['country']);
+    $stmt->bind_param("ssssss", $reg_key, $form["email"], $form["firstName"], $form["lastName"], $form["dateOfBirth"], $form["country"]);
     if (!$stmt->execute()) {
         http_response_code(500);
         echo "Error processing request.";
