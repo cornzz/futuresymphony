f12be0b8257e6f5a882f503c7e75960a1a5b9bd0
cornzz <39997278+cornzz@users.noreply.github.com>
2022-03-08 17:49:07 +0100

    Rebuild

---
+++ build/api/cron_cleanup.php
@@ -0,0 +1,14 @@
+<?php
+    include "db_connection.php";
+
+    if (!isset($_GET["token"]) || $_GET["token"] != CRONJOB_TOKEN) {
+        http_response_code(403);
+        echo "Invalid token.";
+        return;
+    }
+
+    $conn = OpenCon();
+
+    $delay = time() - 60 * 5;
+    $conn->query("DELETE FROM request_addr WHERE timestamp < $delay");
+?>
