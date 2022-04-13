48eb228379db20da1d3842d70138dfefff8c0cfa
cornzz <39997278+cornzz@users.noreply.github.com>
2022-04-13 16:00:53 +0200

    Order registrations by id descending

--- static/api/admin.php
+++ static/api/admin.php
@@ -69,6 +69,9 @@
                             targets: "_all",
                             className: 'dt-center'
                         }
+                    ],
+                    "order": [
+                        [ 0, "desc" ]
                     ]
                 });
                 $('#table2').DataTable({
@@ -77,6 +80,9 @@
                             targets: "_all",
                             className: 'dt-center'
                         }
+                    ],
+                    "order": [
+                        [ 0, "desc" ]
                     ]
                 });
             });
