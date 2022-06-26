17fa31450adcd2d4610b4fdbe0fb4b9fed718d60
cornzz <39997278+cornzz@users.noreply.github.com>
2022-06-26 21:34:14 +0200

    Fix base registration table fixture

--- db/fixtures/base_registrations.sql
+++ db/fixtures/base_registrations.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c0eb440be4f2adddb8fbb1ed76be53e734d0f43b388b65843cf23d2b443165b7
-size 1757
+oid sha256:55f55baa3b21a1e86d2ad3c9bbd4337b3d4ae85cc15e3e5fe8632ef50de07994
+size 1756
