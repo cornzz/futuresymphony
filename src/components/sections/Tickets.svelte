4a088dca7af1f62b47df163daad595bfac56e362
cornzz <39997278+cornzz@users.noreply.github.com>
2022-08-10 02:08:42 +0200

    Add tickets link, update description

--- src/components/sections/Tickets.svelte
+++ src/components/sections/Tickets.svelte
@@ -5,9 +5,9 @@
 
 <div class="content">
     <h1 class="cover-heading tickets">{$_('tickets.title')}</h1>
-    {$_('tickets.caption')}
+    {@html $_('tickets.caption')}
     <div class="center">
-        <a class="button" href="https://google.com" target="_blank">
+        <a class="button" href={$_('tickets.href')} target="_blank">
             <Button type="primary" style="padding: 0 20px">
                 {$_('tickets.button')}
             </Button>
@@ -17,24 +17,28 @@
 
 <style lang="stylus">
     .content
-        line-height 1.65
         text-align center
         background #e9e9e9
         border-radius var(--border-radius)
-        margin 20px 0 15px
+        width 90%
+        margin 20px auto 15px
         padding 0 50px
         border solid 1px var(--color-primary)
 
         .center
             display flex
-            margin 30px 0 40px
+            margin 20px 0 25px
             justify-content center
 
     @media screen and (max-device-width: 600px)
         .content
-            margin-bottom 0
-            padding 0 25px
+            width 100%
+            margin-bottom 5px
+            padding 0 15px
+
+            h1
+                margin-top 15px
 
             .center
-                margin 25px 0 30px
+                margin 15px 0 20px
 </style>
