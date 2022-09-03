e35227ab06e44d07fba37575b234a2b1a3ebd42c
cornzz <39997278+cornzz@users.noreply.github.com>
2022-09-03 15:48:52 +0300

    Update contacts design

--- src/components/sections/Contacts.svelte
+++ src/components/sections/Contacts.svelte
@@ -8,41 +8,41 @@
 
 <div class="content">
     <b>{$_('contacts.organizer')}:</b>
-    <div>
+    <div class="contacts">
         {$_('contacts.vsi')} „Muzikinis pirštas“
     </div>
     <b>{$_('contacts.email')}:</b> 
-    <div>
-        <b><span on:click="{mail}" class="contactlink mail" data-user="info"></span></b>
+    <div class="contacts">
+        <b><span on:click={mail} class="contactlink mail" data-user="info"></span></b>
     </div>
     <b>{$_('contacts.projectManagement')}:</b>
-    <div>
+    <div class="contacts">
         Adomas Morkūnas-Budrys &ndash;
-        <span on:click="{mail}" class="contactlink mail" title={$_('contacts.sendEmail')} data-user="adomas"></span><br>
+        <span on:click={mail} class="contactlink mail" title={$_('contacts.sendEmail')} data-user="adomas"></span><br>
         Danielius Tuita &ndash;
-        <span on:click="{mail}" class="contactlink mail" title={$_('contacts.sendEmail')}  data-user="danielius"></span><br>
+        <span on:click={mail} class="contactlink mail" title={$_('contacts.sendEmail')}  data-user="danielius"></span><br>
         Jurgis Kubilius &ndash;
-        <span on:click="{mail}" class="contactlink mail" title={$_('contacts.sendEmail')}  data-user="jurgis"></span>
+        <span on:click={mail} class="contactlink mail" title={$_('contacts.sendEmail')}  data-user="jurgis"></span>
     </div>						
     <b>{$_('contacts.orchestraManagement')}:</b>
-    <div>
+    <div class="contacts">
         Džiugas Daugirda &ndash;
-        <span on:click="{mail}" title={$_('contacts.sendEmail')}  class="contactlink mail" data-user="dziugas"></span>
+        <span on:click={mail} title={$_('contacts.sendEmail')}  class="contactlink mail" data-user="dziugas"></span>
     </div>
     <b>{$_('contacts.communicationAssistance')}:</b>
-    <div>
+    <div class="contacts">
         Salomėja Petronytė &ndash;
-        <span on:click="{mail}" title={$_('contacts.sendEmail')}  class="contactlink mail" data-user="salomeja"></span>
+        <span on:click={mail} title={$_('contacts.sendEmail')}  class="contactlink mail" data-user="salomeja"></span>
     </div>
     <b>{$_('contacts.socialMedia')}:</b>
-    <div>
+    <div class="contacts">
         <a href="https://www.facebook.com/FScompetition" class="link" target="_blank">Facebook - Future Symphony Competition</a><br>
         <a href="https://www.youtube.com/channel/UCMQ-o5ylqM9DBZfNiD6bVGA/videos" class="link" target="_blank">YouTube - Future Symphony Competition</a>
     </div>
     <b>{$_('contacts.webdesign')}:</b>
-    <div>
+    <div class="contacts">
         Cornelius Kummer &ndash;
-        <span on:click="{mail}" title={$_('contacts.sendEmail')}  class="contactlink mail" data-user="cornelius"></span><br>
+        <span on:click={mail} title={$_('contacts.sendEmail')}  class="contactlink mail" data-user="cornelius"></span><br>
         <a href="https://corny.me" class="link" target="_blank">www.corny.me</a>
     </div>
 </div> 
@@ -52,8 +52,22 @@
         line-height: 1.65
         flex-grow 1
 
-        & > div
-            padding: 0 0 5px 30px
+        .contacts
+            position relative
+            padding-left 30px
+            margin 5px 0
+
+            &:before
+                content ''
+                position absolute
+                top 0
+                left 14px
+                height 100%
+                width 2px
+                padding 2px 0
+                border-radius 3px
+                background var(--color-primary)
+                box-sizing border-box
 
             .contactlink
                 color inherit
